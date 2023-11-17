3c14301fa48f9d479a2dac6cb1b30dfd45a39a4a
soubj <sobujahmed124@gmail.com>
2023-11-17 21:28:18 +0600

    created banner

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer/Footer";
+import NavBar from "../pages/shared/NavBar/NavBar";
 
 const Main = () => {
   return (
     <>
+      <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
     </>
