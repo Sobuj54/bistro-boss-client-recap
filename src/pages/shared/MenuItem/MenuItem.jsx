11a7892c76981c50531290cd7f8d0b6fa6114b4d
soubj <sobujahmed124@gmail.com>
2023-11-17 22:57:37 +0600

    created popular menu item

---
+++ src/pages/shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MenuItem = ({ item }) => {
+  const { name, image, recipe, price } = item;
+
+  return (
+    <div className="flex gap-4">
+      <img
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-[100px]"
+        src={image}
+        alt=""
+      />
+      <div>
+        <h3 className="text-2xl">{name}</h3>
+        <p>{recipe}</p>
+      </div>
+      <p className="text-yellow-500">${price}</p>
+    </div>
+  );
+};
+
+export default MenuItem;
