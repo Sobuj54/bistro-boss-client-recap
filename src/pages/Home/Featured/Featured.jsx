1fe75f8f0e1b201eadaafc275b4c9b44d8e5981d
soubj <sobujahmed124@gmail.com>
2023-11-18 21:02:03 +0600

    featured items with parallax

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured-background bg-fixed text-white py-10 my-20">
+      <SectionTitle
+        subHeading="Check it Out"
+        heading="Featured Item"></SectionTitle>
+
+      <div className="md:flex justify-center items-center gap-10 px-36 py-32">
+        <div>
+          <img className="rounded-lg" src={featuredImg} alt="" />
+        </div>
+        <div>
+          <p>20 Aug, 2024</p>
+          <p className="uppercase">Where can i get some ?</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            repellat quia nisi, neque unde doloribus maiores laborum! Mollitia,
+            similique ab!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
