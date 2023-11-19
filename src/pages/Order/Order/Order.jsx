4077d0e4f46bb201e000353af3715fbe1df08e8b
soubj <sobujahmed124@gmail.com>
2023-11-19 21:03:47 +0600

    created tab system

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Cover from "../../shared/Cover/Cover";
+import orderImg from "../../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
+
+const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const drinks = menu.filter((item) => item.category === "drinks");
+  const desserts = menu.filter((item) => item.category === "dessert");
+
+  return (
+    <div>
+      <Cover img={orderImg} title="Order Food"></Cover>
+
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+
+        <TabPanel>
+          <OrderTab items={salad}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizza}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={desserts}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}></OrderTab>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Order;
